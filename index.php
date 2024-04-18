295e7b1a7e976576202afaa0fab0df06ce3658b5
simoneevacca <v.simone.vacca@gmail.com>
2024-04-18 14:18:36 +0200

    add html structure

--- index.php
+++ index.php
@@ -4,9 +4,26 @@ require_once __DIR__ . '/Models/Production.php';
 
 $starWors = new Production('Star Wars', 'English', 9);
 $killBill = new Production('Kill Bill', 'English', 7);
+$kinkLion = new Production('The King Lion', 'English', 6);
 
 
 
 
-echo $starWors->getData();
-echo $killBill->getData();
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div><?=$starWors->getData();?></div>
+    <div><?=$killBill->getData();?></div>
+    <div><?=$kinkLion->getData();?></div>
+
+    
+
+</body>
+</html>
